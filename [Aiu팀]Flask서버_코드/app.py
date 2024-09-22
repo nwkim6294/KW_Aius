ba42ce47b39f607501e09f6c2f5c88539b31883f
김정원 <53960185+garden-kim-git@users.noreply.github.com>
2024-09-23 04:11:28 +0900

    FLASK code update

--- [Aiu팀]Flask서버_코드/app.py
+++ [Aiu팀]Flask서버_코드/app.py
@@ -15,15 +15,9 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 def run_periodic_update_task():
     while True:
         with app.app_context():  # Flask 애플리케이션 컨텍스트 설정
-            # update_task 먼저 실행
             print("Starting update task")
             update_task()
-            time.sleep(30)  # 30초 대기 후
-
-            # predict_task 실행
-            print("Starting predict task")
-            predict_task()
-            time.sleep(30)  # 다시 30초 대기
+            time.sleep(45)
 
 if __name__ == '__main__':
     # 백그라운드에서 주기적으로 작업을 실행할 스레드 시작
@@ -32,4 +26,4 @@ if __name__ == '__main__':
     task_thread.start()
 
     # Flask 앱 실행
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", use_reloader=False)
